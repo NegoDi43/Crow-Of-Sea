6aed980cb940f2ee99e933f31086252ec3e1c525
Luis Gustavo <pankadinhagdf@gmail.com>
2025-11-06 14:20:02 -0300

    Documentei o ItemInteraction2D

--- Assets/Scripts/UI/ItemInteraction2D.cs
+++ Assets/Scripts/UI/ItemInteraction2D.cs
@@ -2,38 +2,38 @@ using UnityEngine;
 
 public class ItemInteraction2D : MonoBehaviour
 {
-    [Header("Informações do Item")]
-    public string itemNome;
-    [TextArea(2, 5)]
-    public string itemDescricao;
+    //[Header("Informações do Item")]
+    //public string itemNome;
+    //[TextArea(2, 5)]
+    //public string itemDescricao;
 
-    private bool jogadorPerto = false;
+    //private bool jogadorPerto = false;
 
-    void OnTriggerEnter2D(Collider2D outro)
-    {
-        if (outro.CompareTag("Player"))
-        {
-            jogadorPerto = true;
-            UIItemInfo2D.instancia.MostrarInfo(this);
-        }
-    }
+    //void OnTriggerEnter2D(Collider2D outro)
+    //{
+    //    if (outro.CompareTag("Player"))
+    //    {
+    //        jogadorPerto = true;
+    //        UIItemInfo2D.instancia.MostrarInfo(this);
+    //    }
+    //}
 
-    void OnTriggerExit2D(Collider2D outro)
-    {
-        if (outro.CompareTag("Player"))
-        {
-            jogadorPerto = false;
-            UIItemInfo2D.instancia.EsconderInfo();
-        }
-    }
+    //void OnTriggerExit2D(Collider2D outro)
+    //{
+    //    if (outro.CompareTag("Player"))
+    //    {
+    //        jogadorPerto = false;
+    //        UIItemInfo2D.instancia.EsconderInfo();
+    //    }
+    //}
 
-    public void PegarItem()
-    {
-        if (jogadorPerto)
-        {
-            Debug.Log("Item pego: " + itemNome);
-            UIItemInfo2D.instancia.EsconderInfo();
-            Destroy(gameObject);
-        }
-    }
+    //public void PegarItem()
+    //{
+    //    if (jogadorPerto)
+    //    {
+    //        Debug.Log("Item pego: " + itemNome);
+    //        UIItemInfo2D.instancia.EsconderInfo();
+    //        Destroy(gameObject);
+    //    }
+    //}
 }
